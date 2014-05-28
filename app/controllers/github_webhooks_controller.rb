0e6c22ea9d0ac0287a2ae8f3235d6502da2a7b66
ian asaff <ian.asaff@gmail.com>
2014-05-28 16:34:46 -0400

    listener_for

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,4 +1,5 @@
 class GithubWebhooksController < ApplicationController
+  skip_before_action  :verify_authenticity_token
   def ping
     render json: {
       status: :ok
@@ -14,7 +15,14 @@ class GithubWebhooksController < ApplicationController
   end
 
   private
-  def listen_for
-    %w(pull_request pull_request_review_comment)
+  def listener_for(event)
+    # pull_request, push happens on merge
+    # delete to delete a branch
+    #%w(pull_request pull_request_review_comment issue_comment)
+    {
+      'pull_request' => 'A PULL REQUEST WAS OPENED!!',
+      'pull_request_review_comment' => 'A COMMENT WAS MADE ON YOUR PR!',
+      'issue_comment' => 'SOMEONE COMMENTED ON YOUR OPEN PR'
+    }
   end
 end
