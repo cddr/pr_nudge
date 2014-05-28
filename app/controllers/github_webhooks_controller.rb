10c3f47e455cf717e18f80bb5bab9c85af4933ad
ian asaff <ian.asaff@gmail.com>
2014-05-28 15:46:26 -0400

    poc

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,2 +1,13 @@
 class GithubWebhooksController < ApplicationController
+  def ping
+    render json: {
+      status: :ok
+    }, status: :ok
+  end
+
+  def payload
+    push = JSON.parse(params[:github_webhook].to_json)
+    puts "I got some JSON: #{push.inspect}"
+    render json: push
+  end
 end
